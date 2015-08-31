1c31057806462ce17f7638165349c4440a671439
Henning Verbeek <hv@skalio.com>
2015-08-31 13:05:32 +0200

    Fixes general error detection when parsing the scan-response
    When ClamAV reports an error, the esponses always end with "ERROR".

--- src/main/java/com/philvarner/clamavj/ScanResult.java
+++ src/main/java/com/philvarner/clamavj/ScanResult.java
@@ -12,9 +12,9 @@ public class ScanResult {
     public static final String STREAM_PREFIX = "stream: ";
     public static final String RESPONSE_OK = "stream: OK";
     public static final String FOUND_SUFFIX = "FOUND";
+    public static final String ERROR_SUFFIX = "ERROR";
 
     public static final String RESPONSE_SIZE_EXCEEDED = "INSTREAM size limit exceeded. ERROR";
-    public static final String RESPONSE_ERROR_WRITING_FILE = "Error writing to temporary file. ERROR";
 
     public ScanResult(String result) {
         setResult(result);
@@ -46,9 +46,7 @@ public class ScanResult {
             setStatus(Status.PASSED);
         } else if (result.endsWith(FOUND_SUFFIX)) {
             setSignature(result.substring(STREAM_PREFIX.length(), result.lastIndexOf(FOUND_SUFFIX) - 1));
-        } else if (RESPONSE_SIZE_EXCEEDED.equals(result)) {
-            setStatus(Status.ERROR);
-        } else if (RESPONSE_ERROR_WRITING_FILE.equals(result)) {
+        } else if (result.endsWith(ERROR_SUFFIX)) {
             setStatus(Status.ERROR);
         }
 
